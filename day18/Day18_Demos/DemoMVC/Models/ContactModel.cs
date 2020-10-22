300382a077aa69408d644372beffbdb121e99a52
Rahul <rahuls@sunbeaminfo.com>
2020-10-22 14:44:32 +0530

    Lecture contents uploaded

---
+++ day18/Day18_Demos/DemoMVC/Models/ContactModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMVC.Models
+{
+    public class ContactModel
+    {
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string EMail { get; set; }
+        public string Query { get; set; }
+
+        public override string ToString()
+        {
+         return  string.Format("Hello {0}, We have received your query as per details:Name: {0}, Phone: {1}, EMail:{2}, Query:{3} ",
+                this.Name, this.Phone,this.EMail, this.Query);
+        }
+    }
+}
